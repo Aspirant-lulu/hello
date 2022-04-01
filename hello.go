ad30002b629c4286d92f20fac1fbbcea60b7079e
malulu <malulu@sharetek.com.cn>
2022-04-01 22:03:43 +0800

    SayHi函数新增参数

--- hello.go
+++ hello.go
@@ -3,6 +3,6 @@ package hello
 import "fmt"
 
 // SayHi 一个打招呼的函数
-func SayHi(name string) {
-	fmt.Printf("你好%s，我是帅哥。很高兴认识你~\n", name)
+func SayHi(name string, age int) {
+	fmt.Printf("你好%s，我是帅哥。今年%d岁，很高兴认识你~\n", name, age)
 }
